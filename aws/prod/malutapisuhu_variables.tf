07edc4f9c5bf530dcc4e51b26338a974917dd43a
Manda Putra <mandaputra8@gmail.com>
2025-01-29 22:24:35 +0700

    feat: change database for development

--- aws/prod/malutapisuhu_variables.tf
+++ aws/prod/malutapisuhu_variables.tf
@@ -16,7 +16,7 @@ variable "malutapisuhu_databases" {
 variable "malutapisuhu_database_configs" {
   default = {
     name          = "malutapisuhu-db",
-    instance_type = "db.t4g.large",
+    instance_type = "db.t4g.micro",
     db_name       = "malutapisuhu-tutuplapak"
   }
 }
